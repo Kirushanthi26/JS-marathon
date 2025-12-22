a1bf2a0b225e5668a7ff3bae36665a3a45dc24a3
Kirushanthi26 <kirushanthikiru26@gmail.com>
2025-12-22 13:53:40 +0530

    get an array of names from given array of users

---
+++ 01-JsInterviewQnsP1.js
@@ -0,0 +1,25 @@
+// Q1. Write code to get an array of names from given array of users
+
+const users = [
+{id: 1, name: 'Jack', isActive: true},
+{id: 2, name: 'John', isActive: true},
+{id: 3, name: 'Mike', isActive: false},
+]
+
+//way1
+// for (const user of users) {
+//     console.log(user.name)
+// }
+
+//way2
+// const nameSet = []
+// for (let i = 0; i < users.length; i++) {
+//     nameSet.push(users[i].name)
+// }
+// console.log(nameSet)
+
+//way3 - good way
+const nameSet = users.map(user => user.name)
+console.log(nameSet)
+
+//--------------------------------------------------------------------------------------
