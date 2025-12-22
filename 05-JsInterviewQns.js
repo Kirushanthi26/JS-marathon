f1ffe05065d997ad7492ca6a82aa7dcec17a9b89
Kirushanthi26 <kirushanthikiru26@gmail.com>
2025-12-22 23:00:18 +0530

    fibonacci series

--- 05-JsInterviewQns.js
+++ 05-JsInterviewQns.js
@@ -20,7 +20,18 @@ console.log(fizzBuzz(15))
 
 //------------------------------------------------------
 
-//Q2 - fibonacci series
+//Q2 - fibonacci series - 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+function fibonacci(n) {
+  const result = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    result[i] = result[i - 1] + result[i - 2];
+  }
+
+  return result;
+}
+
+console.log(fibonacci(10));
 
 //------------------------------------------------------
 
